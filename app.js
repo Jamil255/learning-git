173f37bb2f25456c3944e60c07d6f9786a8ec809
unknown <jamilafzal255@gmail.com>
2024-05-28 23:33:25 +0500

    const a

--- app.js
+++ app.js
@@ -3,5 +3,8 @@ console.log('git clone')
 console.log('git checkout')
 console.log('git branch')
 console.log('git init ')
-console.log('git add ',()=>'master')
-console.log('git add ',(button))
+console.log('git add ', () => 'master')
+console.log('git add ', button)
+
+const a = 12
+console.log(a)
